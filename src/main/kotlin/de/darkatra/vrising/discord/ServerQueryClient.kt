eb01cee64aa2392ebb2cab9f9c77c80c193b2fa5
DarkAtra <darkatra@web.de>
2022-07-07 19:18:53 +0200

    chore: use the default executor of agql-source-query for server queries

--- src/main/kotlin/de/darkatra/vrising/discord/ServerQueryClient.kt
+++ src/main/kotlin/de/darkatra/vrising/discord/ServerQueryClient.kt
@@ -7,23 +7,19 @@ import com.ibasco.agql.protocols.valve.source.query.SourceQueryClient
 import com.ibasco.agql.protocols.valve.source.query.SourceQueryOptions
 import com.ibasco.agql.protocols.valve.source.query.info.SourceServer
 import com.ibasco.agql.protocols.valve.source.query.players.SourcePlayer
-import org.springframework.beans.factory.DisposableBean
 import org.springframework.stereotype.Service
 import java.net.InetSocketAddress
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 @Service
-class ServerQueryClient : DisposableBean {
+class ServerQueryClient {
 
-    private val executor: ExecutorService = Executors.newCachedThreadPool()
     private val queryOptions = SourceQueryOptions.builder()
+        .option(GeneralOptions.CONNECTION_POOLING, true)
         // FIXME: failsafe causes issues when running the bot as jar, (1) disable it for now
         .option(ConnectOptions.FAILSAFE_ENABLED, false)
         .option(FailsafeOptions.FAILSAFE_ENABLED, false)
         // FIXME: when disabling failsafe, this option is not set for some reason. (2) explicitly set it to 5 seconds (the default)
         .option(GeneralOptions.WRITE_TIMEOUT, 5000)
-        .option(GeneralOptions.THREAD_EXECUTOR_SERVICE, executor)
         .build()
 
     fun getServerInfo(serverHostName: String, serverQueryPort: Int): SourceServer {
@@ -45,9 +41,5 @@ class ServerQueryClient : DisposableBean {
         return SourceQueryClient(queryOptions).use { client ->
             client.getRules(address).join().result
         }
-    }
-
-    override fun destroy() {
-        executor.shutdown()
     }
 }
