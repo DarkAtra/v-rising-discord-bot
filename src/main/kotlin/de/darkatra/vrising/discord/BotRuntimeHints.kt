f6c41a8f41f6a1a1fefbd2496f761efc7b8585f2
Tommy Schmidt <darkatra@gmail.com>
2023-04-23 20:16:16 +0200

    fix: add runtime hints for kord until kordlib/kord/issues/786 is merged (#67)

--- src/main/kotlin/de/darkatra/vrising/discord/BotRuntimeHints.kt
+++ src/main/kotlin/de/darkatra/vrising/discord/BotRuntimeHints.kt
@@ -1,5 +1,21 @@
 package de.darkatra.vrising.discord
 
+import dev.kord.core.cache.data.ApplicationCommandData
+import dev.kord.core.cache.data.AutoModerationRuleData
+import dev.kord.core.cache.data.ChannelData
+import dev.kord.core.cache.data.EmojiData
+import dev.kord.core.cache.data.GuildApplicationCommandPermissionsData
+import dev.kord.core.cache.data.GuildData
+import dev.kord.core.cache.data.MemberData
+import dev.kord.core.cache.data.MessageData
+import dev.kord.core.cache.data.PresenceData
+import dev.kord.core.cache.data.RoleData
+import dev.kord.core.cache.data.StickerData
+import dev.kord.core.cache.data.StickerPackData
+import dev.kord.core.cache.data.ThreadMemberData
+import dev.kord.core.cache.data.UserData
+import dev.kord.core.cache.data.VoiceStateData
+import dev.kord.core.cache.data.WebhookData
 import io.ktor.network.selector.InterestSuspensionsMap
 import io.ktor.utils.io.pool.DefaultPool
 import org.dizitart.no2.Document
@@ -8,6 +24,7 @@ import org.dizitart.no2.NitriteId
 import org.dizitart.no2.meta.Attributes
 import org.h2.store.fs.FilePathDisk
 import org.h2.store.fs.FilePathNio
+import org.springframework.aot.hint.BindingReflectionHintsRegistrar
 import org.springframework.aot.hint.MemberCategory
 import org.springframework.aot.hint.RuntimeHints
 import org.springframework.aot.hint.RuntimeHintsRegistrar
@@ -20,6 +37,8 @@ import java.util.concurrent.atomic.AtomicBoolean
  * Runtime hints for dependencies. Should be removed when each dependency has official support for GraalVM Native Image.
  */
 class BotRuntimeHints : RuntimeHintsRegistrar {
+
+    private val bindingReflectionHintsRegistrar = BindingReflectionHintsRegistrar()
 
     override fun registerHints(hints: RuntimeHints, classLoader: ClassLoader?) {
 
@@ -42,10 +61,32 @@ class BotRuntimeHints : RuntimeHintsRegistrar {
         hints.serialization().registerType(NitriteId::class.java)
         hints.serialization().registerType(TypeReference.of("java.lang.String"))
 
+        // reflection hints for kord (remove once https://github.com/kordlib/kord/issues/786 is merged)
+        bindingReflectionHintsRegistrar.registerReflectionHints(
+            hints.reflection(),
+            ApplicationCommandData::class.java,
+            AutoModerationRuleData::class.java,
+            ChannelData::class.java,
+            EmojiData::class.java,
+            GuildData::class.java,
+            MemberData::class.java,
+            MessageData::class.java,
+            PresenceData::class.java,
+            RoleData::class.java,
+            StickerData::class.java,
+            ThreadMemberData::class.java,
+            UserData::class.java,
+            VoiceStateData::class.java,
+            WebhookData::class.java
+        )
+
         hints.reflection()
             // required by nitrite to create and open file based databases
             .registerType(FilePathDisk::class.java, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
             .registerType(FilePathNio::class.java, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+            // required by kord (remove once https://github.com/kordlib/kord/issues/786 is merged)
+            .registerType(GuildApplicationCommandPermissionsData::class.java)
+            .registerType(StickerPackData::class.java)
             // required by kotlin coroutines (dependency of kord)
             .registerType(TypeReference.of("kotlin.internal.jdk8.JDK8PlatformImplementations"), MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
             // required by ktor (dependency of kord)
