6b7038d1f525d6e3433a69e50d0ddd09fe75fcb6
DarkAtra <darkatra@gmail.com>
2024-05-17 00:03:02 +0200

    chore: reproducer for the timeout issue

--- src/main/kotlin/de/darkatra/vrising/discord/clients/serverquery/ServerQueryClient.kt
+++ src/main/kotlin/de/darkatra/vrising/discord/clients/serverquery/ServerQueryClient.kt
@@ -1,10 +1,13 @@
 package de.darkatra.vrising.discord.clients.serverquery
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
 import com.ibasco.agql.core.util.GeneralOptions
 import com.ibasco.agql.protocols.valve.source.query.SourceQueryClient
 import com.ibasco.agql.protocols.valve.source.query.SourceQueryOptions
 import com.ibasco.agql.protocols.valve.source.query.info.SourceServer
 import com.ibasco.agql.protocols.valve.source.query.players.SourcePlayer
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.DisposableBean
 import org.springframework.stereotype.Service
 import java.net.InetSocketAddress
@@ -42,6 +45,10 @@ class ServerQueryClient : DisposableBean {
 }
 
 fun main() {
+
+    val logger = LoggerFactory.getLogger("com.ibasco.agql") as Logger
+    logger.level = Level.DEBUG
+
     // https://www.battlemetrics.com/servers/vrising/27556679
-    ServerQueryClient().getServerInfo("185.200.246.67", 9877)
+    ServerQueryClient().getRules("185.200.246.67", 9877)
 }
