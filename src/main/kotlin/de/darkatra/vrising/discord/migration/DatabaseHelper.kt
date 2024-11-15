3bdb741917e92bd5848d58f4591a147f7fc9e37a
Tommy Schmidt <tommy.schmidt@idealo.de>
2024-11-15 16:55:35 +0100

    feat: encrypt the bot database at rest

--- src/main/kotlin/de/darkatra/vrising/discord/migration/DatabaseHelper.kt
+++ src/main/kotlin/de/darkatra/vrising/discord/migration/DatabaseHelper.kt
@@ -3,8 +3,15 @@ package de.darkatra.vrising.discord.migration
 import org.dizitart.no2.Nitrite
 import org.dizitart.no2.collection.Document
 import org.dizitart.no2.collection.NitriteId
+import org.dizitart.no2.common.Constants
+import org.dizitart.no2.common.meta.Attributes
 import org.dizitart.no2.store.NitriteMap
 
 fun Nitrite.getNitriteMap(name: String): NitriteMap<NitriteId, Document> {
     return store.openMap(name, NitriteId::class.java, Document::class.java)
+}
+
+fun Nitrite.listAllCollectionNames(): List<String> {
+    return store.openMap<String, Attributes>(Constants.META_MAP_NAME, String::class.java, Attributes::class.java).keys()
+        .filter { key -> !key.startsWith("\$nitrite") }
 }
